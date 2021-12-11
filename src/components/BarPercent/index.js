16af8d01da1c41ffb2bd4528a15306f9874725b9
Javier Benavides <chato.javier@gmail.com>
2021-12-10 20:35:01 -0500

    refactoring to add backend

--- src/components/BarPercent/index.js
+++
@@ -1,9 +0,0 @@
-const BarPercent = ({color, percent}) => {
-    return (
-        <div className="w-full h-2 my-2 rounded-full overflow-hidden bg-gradient-to-b from-gray-200 to-gray-100">
-            <div className={`h-full rounded-full bg-gradient-to-br for ${color ? `from-${color}-600 to-${color}-700` : "from-blue to-blue-dark"}`} style={{width: percent + '%'}}></div>
-        </div>
-    )
-}
-
-export default BarPercent;
--- src/helpers/currencyFormat.js
+++
@@ -1,6 +0,0 @@
-const currencyFormat = (num) => {
-	const fixedNumber = (Math.round(num * 100) / 100).toFixed(2);
-	return String(fixedNumber).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
-
-export default currencyFormat;
